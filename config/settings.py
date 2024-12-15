a70d9ac5b7c531a6fed68ebc1d4dd6009d19b883
Jaxongir <jorajaxongir229@gmail.com>
2024-12-15 17:58:25 +0500

    Add requirements.txt and Procfile

--- config/settings.py
+++ config/settings.py
@@ -67,12 +67,19 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('PGDATABASE'),
+        'USER': os.getenv('PGUSER'),
+        'PASSWORD': os.getenv('PGPASSWORD'),
+        'HOST': os.getenv('PGHOST'),
+        'PORT': os.getenv('PGPORT'),
     }
 }
+
 
 
 # Password validation
